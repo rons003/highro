8f002b5eca18dc24072ad02570d35c6f55f1208f
rons003 <ronelle003@gmail.com>
2020-03-07 20:31:42 +0800

    Update int bragi

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -14623,12 +14623,12 @@ struct skill_unit_group *skill_unitsetting(struct block_list *src, uint16 skill_
 	case BA_POEMBRAGI:
 		val1 = 3 * skill_lv + status->dex / 10; // Casting time reduction
 		//For some reason at level 10 the base delay reduction is 50%.
-		val2 = (skill_lv < 10 ? 3 * skill_lv : 50) + status->int_ / 5; // After-cast delay reduction
-		// if (sd)
-		// {
-		// 	val1 += pc_checkskill(sd, BA_MUSICALLESSON);
-		// 	val2 += 2 * pc_checkskill(sd, BA_MUSICALLESSON);
-		// }
+		val2 = (skill_lv < 10 ? 3 * skill_lv : 50) + status->int_ / 8; // After-cast delay reduction
+		if (sd)
+		{
+			val1 += pc_checkskill(sd, BA_MUSICALLESSON);
+			val2 += 2 * pc_checkskill(sd, BA_MUSICALLESSON);
+		}
 		break;
 	case DC_DONTFORGETME:
 #ifdef RENEWAL
